11da689a20309e820b9d2e10057314bdc4243811
gusmally <hannahmbmoraes@gmail.com>
2022-06-17 00:59:24 +0100

    init

---
+++ divination/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace divination
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
